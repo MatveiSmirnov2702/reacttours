bab402d748361baab6e8bb4b9175a976516c045d
MatveiSmirnov2702 <vvvvmatvvvv@gmail.com>
2022-11-27 14:56:32 +0300

    doing components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import Card from './components/Card';
-import Header from './components/Header';
-import Drawer from './components/Drawer';
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
-
-const arr =[
-  { name: "Тур в бразилию" , price: 124000 },
-  { name: "Тур в мексику" , price: 124000 },
+const arr = [
+  { name: "Тур в бразилию", price: 124000 },
+  { name: "Тур в мексику", price: 124000 },
 ];
-
 
 function App() {
   return (
     <div className="wrapper clear">
-
       <Drawer />
-
       <Header />
       <div className="content p-40">
         <div className="mb-40 align-centre justify-between d-flex">
@@ -25,19 +21,13 @@ function App() {
           </div>
         </div>
         <div className="d-flex">
-          <Card
-            title="Тур в бразилию"
-            price={125000}
-            imageURL="/img/brazilia.jpg"
-           />
-           <Card
-            title="Тур в Мексику"
-            price={250000}
-            imageURL="/img/brazilia.jpg"
-           />
-{/*           {arr.map((obj) => (
-            <Card/>
-          ))} */}
+          {arr.map((obj) => (
+            <Card
+              title="Тур в Мексику"
+              price={250000}
+              imageURL="/img/brazilia.jpg"
+            />
+          ))}
         </div>
       </div>
     </div>
