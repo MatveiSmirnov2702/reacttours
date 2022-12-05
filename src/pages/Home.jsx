86f151f603318d032e05f0f05877978a33bfe061
MatveiSmirnov2702 <vvvvmatvvvv@gmail.com>
2022-12-05 11:06:42 +0300

    05.12

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,7 +44,7 @@ function Home({
               title={item.title}
               onFavorite={(obj) => onAddToFavorite(obj)}
               onPlus={(obj) => onAddToCart(obj)}
-              added={cartItems.some(obj => obj.id === item.id)}
+              added={cartItems.some(obj => Number(obj.id) === item.id)}
               {...item}
             />
           ))}
